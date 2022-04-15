fe892fedcffe2ad0a9c891ffd7c28c8f645046a5
Mariano Tupa <tupa.sales@gmail.com>
2022-04-15 01:01:44 -0300

    feat: :sparkles: Permite que o usuario crie um grupo para estabelecer a porcentagem desejada para cada motorista

--- src/blueprints/revenues/routes.py
+++ src/blueprints/revenues/routes.py
@@ -1,21 +1,67 @@
 """ Motorist Routes """
 
 
-from flask import Blueprint, render_template
-from flask_cors import CORS
-
+from flask import Blueprint, render_template, redirect, request, url_for
+from src.database.querys import RevenuesQuery
 
 revenues_app = Blueprint(
     "revenues_app", __name__,
     url_prefix="/revenues/")
 
-# logging.getLogger('flask_cors').level = logging.DEBUG
-CORS(
-    revenues_app,
-    )
 
-
-@revenues_app.route("/", methods=["GET"])
-def get_motorits():
+@revenues_app.route("/settings", methods=["GET"])
+def show():
     "Get all motorists in database"
-    return render_template("pages/revenues.html")
+    porcents = RevenuesQuery.all()
+    return render_template("pages/revenues/show.html", porcents=porcents)
+
+
+@revenues_app.route("/create", methods=["POST", "GET"])
+def create():
+    "Create a new motorist"
+    if request.method == "POST":
+        group_name = request.form.get("group_name")
+        porcent_one = request.form.get("porcent_one")
+        porcent_two = request.form.get("porcent_two")
+        porcent_tree = request.form.get("porcent_tree")
+        RevenuesQuery.create_motorist_group(
+            group_name,
+            porcent_one,
+            porcent_two,
+            porcent_tree)
+
+        return redirect(url_for("revenues_app.show"))
+
+    return render_template("pages/revenues/create.html")
+
+@revenues_app.route("/edit/<porcent_id>", methods=["POST", "GET"])
+def edit(porcent_id):
+    "Create a new motorist"
+    # if request.method == "POST":
+    #     porcent_one = request.form.get("porcent_one")
+    #     porcent_two = request.form.get("porcent_two")
+    #     porcent_tree = request.form.get("porcent_tree")
+    #     RevenuesQuery.update_porcent(
+    #         porcent_one,
+    #         porcent_two,
+    #         porcent_tree)
+
+        # return redirect(url_for("revenues_app.show"))
+    porcent = RevenuesQuery.find(porcent_id)
+    print(porcent)
+    return render_template("pages/revenues/edit.html", porcent=porcent)
+
+@revenues_app.route("/invoices", methods=["POST", "GET"])
+def invoices():
+    "Create a new motorist"
+    # if request.method == "POST":
+    #     porcent_one = request.form.get("porcent_one")
+    #     porcent_two = request.form.get("porcent_two")
+    #     porcent_tree = request.form.get("porcent_tree")
+    #     RevenuesQuery.update_porcent(
+    #         porcent_one,
+    #         porcent_two,
+    #         porcent_tree)
+
+        # return redirect(url_for("revenues_app.show"))
+    return render_template("pages/revenues/invoices.html")
