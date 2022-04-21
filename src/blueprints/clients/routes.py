0d3311e7b253068de0fda1479f3bc37f152270b2
Mariano Tupa <tupa.sales@gmail.com>
2022-04-21 00:17:08 -0300

    perf: :see_no_evil:

--- src/blueprints/clients/routes.py
+++ src/blueprints/clients/routes.py
@@ -8,6 +8,7 @@ clients_app = Blueprint("clients_app", __name__, url_prefix="/clients/")
 
 @clients_app.route("/", methods=["GET"])
 def show():
-    """Exibe a pagina de clientes"""
+    """Exibe a pagina de clientes
+    """
     
     return render_template("pages/clientes.html")
