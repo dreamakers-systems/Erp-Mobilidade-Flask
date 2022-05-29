413021f759b41309cb48f8037a9bbcf16591f494
Mariano Tupa <tupa.sales@gmail.com>
2022-05-29 01:15:02 -0300

    feat: :art: Creting authentication module in application!

--- src/main.py
+++ src/main.py
@@ -1,44 +1,44 @@
 # pylint: disable=import-outside-toplevel
-""" Aplication Factory """
+"""Aplication Factory"""
 
 from flask import Flask
 
 
 def init_app() -> Flask:
-    """ init_app é uma fectory que retorna 
+    """init_app é uma fectory que retorna
     uma instancia da aplcação Flask.
-    
-    Para configurar a aplicação deve-se Intanciar 
+
+    Para configurar a aplicação deve-se Intanciar
     o objeto de configuração de acordo com o ambiente.
-    
+
     O banco de dodos é manipulado através da biblioteca
     SQLAlchemy. Todos os arquivos para interação com
-    o banco de dados esta em ./src/database/, sendo que 
+    o banco de dados esta em ./src/database/, sendo que
     dentro desse diretorio existe outras duas pastas essenciais,
     que são:
-    ../models que contens os models da aplicão e 
-    ../querys que contem as interações com o banco de dados 
-    
-    
+    ../models que contens os models da aplicão e
+    ../querys que contem as interações com o banco de dados
+
+
     SQLAlchemy - Website: https://www.sqlalchemy.org/
-    
+
     As Blueprints da apicação estão setadas detro de
     ./src/blueprints/blueprint_name/. Cada uma
-    possui um url proprio para suas rotas que é 
-    setado em url_prefix. Ela pode conter um diretorio 
-    src para manter os objetos utilizados pela blueprints 
-    
+    possui um url proprio para suas rotas que é
+    setado em url_prefix. Ela pode conter um diretorio
+    src para manter os objetos utilizados pela blueprints
+
     """
-    
+
     app = Flask(__name__)
 
     # Setando configurações da aplicação
     from .settings import Config
+
     app.config.from_object(Config)
 
     # Configurando banco de dados
-    from .database import DBConnectionHendler
-    from .database import Base
+    from .database import Base, DBConnectionHendler
 
     db_connection = DBConnectionHendler()
     engine = db_connection.get_engine()
@@ -46,19 +46,27 @@ def init_app() -> Flask:
     # Criando um contexto para a aplicação.
     # Referencia: https://flask.palletsprojects.com/en/2.1.x/appcontext/
     with app.app_context():
-        
+
         # Registrando as Blueprints: https://flask.palletsprojects.com/en/2.1.x/blueprints/
 
+        from .blueprints import auth
+
+        app.register_blueprint(auth)
+
         from .blueprints import dashboard_app
+
         app.register_blueprint(dashboard_app)
 
         from .blueprints import motorist_app
+
         app.register_blueprint(motorist_app)
 
         from .blueprints import clients_app
+
         app.register_blueprint(clients_app)
 
         from .blueprints import revenues_app
+
         app.register_blueprint(revenues_app)
 
         # Criando tabelas que não existem e estão
--- src/settings.py
+++ src/settings.py
@@ -1,42 +1,49 @@
 # pylint: disable=too-few-public-methods
-""" Configurações para o projeto 
-Para passar determinadas variaveis e constantes para o sistemas 
-esteremos utilizando objetos com diferentes propriedades para 
-cada ambiente. Para setar esse ambiente va para 
+"""Configurações para o projeto
+Para passar determinadas variaveis e constantes para o sistemas
+esteremos utilizando objetos com diferentes propriedades para
+cada ambiente. Para setar esse ambiente va para
 """
 
 import os
+from os.path import abspath, basename, dirname, join
 
+from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.path.dirname(__name__))
+basedir = abspath(dirname(__name__))
+dotenv_path = join(basedir, '.env')
+load_dotenv(dotenv_path)
 
 
 class Config:
     """Configurações globais para todo o projeto"""
 
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    UPLOAD_FOLDER = "/media"
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOAD_FOLDER = '/media'
     ALLOWED_EXTENSIONS = {
-        "txt",
+        'txt',
     }
+    DATABASE_CONNECTION = os.environ.get('DATABASE_CONNECTION')
 
 
 class DevelopmentConfig(Config):
-    """ Ambiente de desenvolvimento"""
+    """Ambiente de desenvolvimento"""
 
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "dev-data.db")
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
+        basedir, 'dev-data.db'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestingConfig(Config):
-    """ Ambiente de testes """
+    """Ambiente de testes"""
 
     DEBUG = False
     TESTING = True
 
 
 class ProductionConfig(Config):
-    """ Ambiente de produção """
+    """Ambiente de produção"""
 
     DEBUG = False
